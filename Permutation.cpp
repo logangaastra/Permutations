8547f2ad6437592e80129b19f49d2da5c714cbbf
Logan Gaastra <45395030+logangaastra@users.noreply.github.com>
2018-11-30 12:51:26 -0500

    Added Print Functions, Debugged
    Fixed size bugs with Constructors(this time for real)
    Added comment dividers in implementation of Permutation Class
    Implemented getCycles function
    Implemented printCycles
    Implemented operator<<
    Implemented getInverse 
    Renamed printTwoLineNotation to printTwoLine
    Debugged operator* and newly implemented functions
    TODO:
    Implement int getNumberOfCycles()
    Implement bool isDerangement()
    Implement vector<int> getCycleIndex()
    Implement vector<pair<int,int>> transpose() (returns adjacent transposition of the Permutation)
    Implement bool isEven()

--- Permutation.cpp
+++ Permutation.cpp
@@ -20,12 +20,14 @@ using namespace std;
   *   integers, then size = 0
 */
 class Permutation {
+    //PRIVATE MEMBER VARIABLES==================================================
     const int DEFAULT_SIZE = 0;
     const int MINIMUM_ELEMENT = 0;
     unordered_map<int, int> perm;
     int size;
     
 public:
+    //CONSTRUCTORS==============================================================
     /** Default Constructor constructs identity permutation
       */
     Permutation() {
@@ -45,9 +47,12 @@ public:
             size = DEFAULT_SIZE;
             return;
         }
-        for(int i = 0; i < integerMap.size(); ++i)
+        size = 0;
+        for(int i = 0; i < integerMap.size(); ++i) {
             perm.emplace(i, integerMap.at(i));
-            size = integerMap.size();
+            if(i != integerMap.at(i))
+                size = i + 1;
+        }
     }
     /** The above constructor, but takes in arrays of ints instead of vectors of
       *   ints
@@ -67,22 +72,22 @@ public:
             return;
         }
         for(int i = 0; i < cycles.size(); ++i) {
-            if(cycles.at(i).size() <= 1)
+            if(cycles[i].size() <= 1)
                 continue;
-            for(int j = 0; j < cycles.at(i).size(); ++j) {
-                perm.emplace(cycles.at(i).at(j), cycles.at(i).at(
-                    (j + 1) % cycles.at(i).size()));
-                size = max(size, cycles.at(i).at(j));
+            for(int j = 0; j < cycles[i].size(); ++j) {
+                perm.emplace(cycles[i][j], cycles[i]
+                    [(j + 1) % cycles[i].size()]);
+                size = max(size, cycles[i][j] + 1);
             }
         }
     }
-    
+    //OPERATORS=================================================================
     /** Overloads operator[] to return the number mapped to by i
       */
     int operator[](int i) const {
         return (perm.count(i) == 1 ? perm.at(i) : i);
     }
-    /** Overloaded operator * to return a Permutation which is
+    /** Overloaded operator* to return a Permutation which is
       *  the composition of this and rhs
       * Example this = 1->2 2->3 3->4 4->1
       *  rhs = 1->2 2->3 3->1
@@ -94,25 +99,82 @@ public:
         for(int i = 0; i < newSize; ++i) {
             int temp = rhs[i];
             temp = (*this)[temp];
-            
             composition.push_back(temp);
+            
         }
+        return Permutation(composition);
     }
+    /* Overloaded operator<< to return output as specified in printOneLine
+     */
+    ostream& operator<<(ostream& os) {
+        printOneLine(os);
+    }
+    //GETTERS===================================================================
     int getSize() const {
         return size;
     }
-    
-    void printOneLineNotation(ostream& os) {
+    /* Returns a vector of vector of ints with each vector representing a cycle
+     * This is the unique cyclical form of Permutation, with the least integer
+     *   in a cycle being the first listed and the cycles listed in order of
+     *   least first entry
+     * Does not include singletons i.e. 1-cycles if includeSingletons is false
+     * Example: given 1->3 2->1 3->2 4->4 5->6 6->5 and false
+     *   will return {1,3,2},{5,6}
+         given 1->3 2->1 3->2 4->4 5->6 6->5 and true
+         will return {1,3,2},{4},{5,6}
+     */
+     vector<vector<int>> getCycles(bool includeSingletons) const {
+        //Keeps track of which numbers have been used already
+        unordered_map<int, bool> alreadyUsed;
+        vector<vector<int>> cycles;
+        for(int i = 0; i < size; ++i) {
+            //Check for singletons
+            if(i == (*this)[i] && !includeSingletons)
+                continue;
+            if(alreadyUsed.count(i) == 1)
+                continue;
+            vector<int> newCycle;
+            newCycle.push_back(i);
+            alreadyUsed.emplace(i, true);
+            int nextNumber = (*this)[i];
+            while(nextNumber != i) {
+                newCycle.push_back(nextNumber);
+                alreadyUsed.emplace(nextNumber, true);
+                nextNumber = (*this)[nextNumber];
+                
+            }
+            cycles.push_back(newCycle);
+        }
+        return cycles;
+    }
+    /* Returns the Permutation that when composed with this permutation
+     *   returns the identity, i.e. the permutation that maps every integer
+     *   onto itself.
+     * Example: (2, 3, 1) returns (3, 1, 2)
+     */
+    Permutation getInverse() const {
+        vector<int> inverseMap = vector<int>(size, -1);
+        for(int i = 0; i < size; ++i)
+            inverseMap[(*this)[i]] = i;
+    }
+    //PRINT FUNCTIONS===========================================================
+    /* Prints the Permutation in one line notation
+     * Does not include newLine at the end
+     */
+    void printOneLine(ostream& os) {
         if(size == 0) {
             os << "()";
             return;
         }
         os << "(" << (*this)[0];
         for(int i = 1; i < size; ++i) 
-            os << setw(maximumNumberOfSpaces()) << (*this)[i];
+            os << ", " << (*this)[i];
         os << ")";
     }
-    void printTwoLineNotation(ostream &os) {
+    /* prints the Permutation in Two line notation 
+     * Takes up 2 lines(obviously) and includes a newLine at the end
+     */
+    void printTwoLine(ostream &os) {
         if(size == 0) {
             os << "[]\n[]\n";
             return;
@@ -125,8 +187,20 @@ public:
             os << setw(maximumNumberOfSpaces()) << (*this)[i];
         os << "]" << endl;
     }
+    /* Prints to os the cycles as specified in getCycles
+     * Does not print newLine afterwards
+     */
+    void printCycles(ostream &os, bool includeSingletons) {
+        vector<vector<int>> cycles = getCycles(includeSingletons);
+        for(int i = 0; i < cycles.size(); ++i) {
+            os << "(" << cycles[i][0];
+            for(int j = 1; j < cycles[i].size(); ++j)
+                os << " " << cycles[i][j];
+            os << ")";
+        }
+    }
 private:
-
+    //PRIVATE MEMBER FUNCTIONS==================================================
     /** Checks that integerMap satisfies the following:
       * 1. all entries are contained in [0,integerMap.size() - 1]
       * 2. all non negative integers less than integerMap.size() are
